8cb4dd7d65c7bc44b89f071d019654f429e22693
bhnbhushan <babar.bhushan@gmail.com>
2021-03-29 19:32:07 +0530

    initial commit: completed innoloft frontend dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+import HomePage from './pages/homepage/homepage.component';
+import ProductPage from './pages/product/product.component';
+import Header from './components/header/header.component';
+import SidebarMenu from './components/sidebar-menu/sidebar-menu.component';
+
+import { fetchConfig } from './redux/config/config.actions';
+import WithSpinner from './components/with-spinner/with-spinner.component';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    const { fetchConfig } = this.props;
+    const appId = process.env.REACT_APP_APP_ID || 1;
+    fetchConfig(appId);
+  }
+
+  render() {
+    const { isLoading } = this.props;
+    const App = () => (
+      <div className="container">
+        <Header />
+        <div className="main">
+          <SidebarMenu />
+          <div className="dashboard">
+            <Switch>
+              <Route exact path='/' component={HomePage}></Route>
+              <Route path='/' component={ProductPage}></Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+    const AppWithSpinner = WithSpinner(App);
+    return (
+      <>
+        <AppWithSpinner isLoading={isLoading} />
+      </>
+    )
+  };
 }
 
-export default App;
+const mapStateToProps = state => ({
+  isLoading: state.config.isFetchingConfig
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchConfig: appId => dispatch(fetchConfig(appId))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
